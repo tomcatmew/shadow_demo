5809f9fe92fca38f85a59c928b2cfb76138b5b07
tomcatmew <helltomcat@gmail.com>
2021-10-17 03:32:35 +0900

    up

--- docs/index.js
+++ docs/index.js
@@ -1,15 +1,27 @@
 var trace1 = {
-  x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-  y: [8, 7, 6, 5, 4, 3, 2, 1, 0],
+  x: [],
+  y: [],
   type: 'scatter'
 };
+
+const fileUrl = 'training_record/train_walk_256_1' // provide file location
+
+fetch(fileUrl)
+  .then(response => response.text())
+  .then(data => {
+  	// Do something with your data
+  	console.log(data);
+  	trace1.x = data.split('\n')[0];
+  	trace1.y = data.split('\n')[1];
+  });
+
 
 var trace2 = {
   x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
   y: [0, 1, 2, 3, 4, 5, 6, 7, 8],
   type: 'scatter'
 };
-
+console.log(trace2.x);
 var data = [trace1, trace2];
 
 var layout = {
@@ -22,14 +34,5 @@ var layout = {
     autorange: true
   }
 };
-
-const fileUrl = 'training_record/train_walk_256_1' // provide file location
-
-fetch(fileUrl)
-  .then(response => response.text())
-  .then(data => {
-  	// Do something with your data
-  	console.log(data);
-  });
 
 Plotly.newPlot('myDiv', data, layout);
